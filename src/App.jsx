e81f7c22c4b4b7892b0a062fc10bb22ac3b103fa
adegbenro ezekiel <eazyplus762@gmail.com>
2026-06-04 22:29:53 +0100

    add deploy config

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Text from './text'
+
+function App() {
+  return (
+    <div className='bg-black h-screen '>
+    <Text/>  
+    </div>
+  )
+}
+
+export default App
